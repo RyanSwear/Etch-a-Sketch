6d9d3e37eaf0368d3d61da861e86d09fff7a463a
Swear <ryanmswearingen@gmail.com>
2020-10-14 22:35:53 -0700

    added sketch board, init 16 x 16

---
+++ script.js
@@ -0,0 +1,18 @@
+const container = document.querySelector('#board');
+
+const grid = document.createElement('div');
+grid.style.color = 'blue';
+grid.classList.add('grid');
+grid.style.display = 'grid';
+grid.style.width = "480px";
+grid.style.height = "480px";
+grid.style.border = "2px solid black";
+grid.style.gridTemplateColumns = 'repeat(16, 1fr [col-start])';
+grid.style.gridTemplateRows = 'repeat(16, 1fr [row-start])';
+for (let i = 0; i < (16*16); i++)
+{
+    const square = document.createElement('div');
+    square.style['background-color'] = 'gray';
+    grid.appendChild(square);
+}
+container.appendChild(grid);
